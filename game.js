02ddeca944da852badbc2608a5f9b1dcca7f4b27
tmoreno <tmorenobernal@yahoo.es>
2015-07-11 16:50:38 +0200

    Added random seed for testing purposes

--- game.js
+++ game.js
@@ -170,6 +170,13 @@ exports.Game = function() {
   };
 };
 
+var randomSeed = require('random-seed');
+var rollRandom = randomSeed.create();
+rollRandom.initState();
+
+var answerRandom = randomSeed.create();
+answerRandom.initState();
+
 var notAWinner = false;
 
 var game = new Game();
@@ -179,10 +186,9 @@ game.add('Pat');
 game.add('Sue');
 
 do{
+  game.roll(rollRandom(6) + 1);
 
-  game.roll(Math.floor(Math.random()*6) + 1);
-
-  if(Math.floor(Math.random()*10) == 7){
+  if(answerRandom(10) + 1 == 7){
     notAWinner = game.wrongAnswer();
   }else{
     notAWinner = game.wasCorrectlyAnswered();
