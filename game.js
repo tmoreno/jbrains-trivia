8cb399c371b3202ab64d951da59fc321c8bce3a7
tmoreno <tmorenobernal@yahoo.es>
2015-07-11 17:59:06 +0200

    Created new var that represent the board game

--- game.js
+++ game.js
@@ -13,31 +13,15 @@ function Game () {
 
   var currentPlayer    = 0;
   var isGettingOutOfPenaltyBox = false;
+    
+    var tablero = ['Pop', 'Science', 'Sports', 'Rock', 'Pop', 'Science', 'Sports', 'Rock', 'Pop', 'Science', 'Sports', 'Rock'];
 
   var didPlayerWin = function(){
     return !(purses[currentPlayer] == 6)
   };
 
-  var currentCategory = function(){
-    if(places[currentPlayer] == 0)
-      return 'Pop';
-    if(places[currentPlayer] == 4)
-      return 'Pop';
-    if(places[currentPlayer] == 8)
-      return 'Pop';
-    if(places[currentPlayer] == 1)
-      return 'Science';
-    if(places[currentPlayer] == 5)
-      return 'Science';
-    if(places[currentPlayer] == 9)
-      return 'Science';
-    if(places[currentPlayer] == 2)
-      return 'Sports';
-    if(places[currentPlayer] == 6)
-      return 'Sports';
-    if(places[currentPlayer] == 10)
-      return 'Sports';
-    return 'Rock';
+  var currentCategory = function() {
+      return tablero[places[currentPlayer]];
   };
 
   for(var i = 0; i < 50; i++){
