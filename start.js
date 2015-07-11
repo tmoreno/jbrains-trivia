1e5afa03c7ceab9ffb628893d3c0861d25a7ff5a
tmoreno <tmorenobernal@yahoo.es>
2015-07-11 17:11:13 +0200

    Pretty print

--- start.js
+++ start.js
@@ -7,20 +7,19 @@ rollRandom.initState();
 var answerRandom = randomSeed.create();
 answerRandom.initState();
 
-var notAWinner = false;
-
 var game = new Game();
 game.add('Chet');
 game.add('Pat');
 game.add('Sue');
 
-do{
-  game.roll(rollRandom(6) + 1);
+var notAWinner = false;
+do {
+    game.roll(rollRandom(6) + 1);
 
-  if(answerRandom(10) + 1 == 7){
-    notAWinner = game.wrongAnswer();
-  }else{
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-
-}while(notAWinner);
+    if(answerRandom(10) + 1 == 7){
+        notAWinner = game.wrongAnswer();
+    }
+    else{
+        notAWinner = game.wasCorrectlyAnswered();
+    }
+} while(notAWinner);
